f0fcd93d4f13f28a79162c23fb8dd4fd858a80c3
Josemiranda989 <josemiranda989@gmail.com>
2025-04-15 17:09:52 -0300

    sprint 7 ok

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -15,4 +15,22 @@ const loginValidator = [
     .withMessage("La contraseña debe tener almenos 5 caracteres"),
 ];
 
-module.exports = { loginValidator };
+const registerValidator = [
+  check("name").notEmpty().withMessage("Debes ingresar un nombre"),
+  check("email")
+    .notEmpty()
+    .withMessage("Debes ingresar un email")
+    .bail()
+    .isEmail()
+    .withMessage("El dato ingresado no corresponde a un email"),
+  check("direction").notEmpty().withMessage("Debes ingresar una direccion"),
+  check("phonenumber")
+    .notEmpty()
+    .withMessage("El campo no puee estar")
+    .bail()
+    .isNumeric()
+    .withMessage("Debes ingresar un numero"),
+  check("password").notEmpty().withMessage("Debes ingresar un password"),
+];
+
+module.exports = { loginValidator, registerValidator };
